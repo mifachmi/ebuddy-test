3c91a798c42b729e8ca9e2faddc292ea8f3eddcd
Fachmi Dimas Ardhana <fachmi2007@gmail.com>
2024-11-27 16:08:34 +0700

    Initial Commit

---
+++ EbuddyTest/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  EbuddyTest
+//
+//  Created by Fachmi Dimas Ardhana on 27/11/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
